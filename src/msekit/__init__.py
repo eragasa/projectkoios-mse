02231a3441ece18f93c3e160a97815f02a4e3187
Eugene J. Ragasa <eugene.ragasa@gmail.com>
2026-05-20 14:10:13 +0800

    first

---
+++ src/msekit/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+msekit: Materials science engineering tools for atomistic and continuum-informed modeling.
+"""
+
+__version__ = "0.0.1"
---
+++ tests/test_import.py
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: Apache-2.0
+
+import msekit
+
+
+def test_import_msekit():
+    assert msekit.__version__ == "0.0.1"
